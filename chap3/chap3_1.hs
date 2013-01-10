b42be09f96498f23995ad1258f81e24a9d3ae674
Suman Nandan <suman.nandan@gmail.com>
2013-01-10 22:48:41 +0530

    completed chapter3

---
+++ chap3/chap3_1.hs
@@ -0,0 +1,7 @@
+-- What are the types of the following values:
+
+['a', 'b', 'c']             :: [Char]
+('a', 'a', 'c')             :: (Char, Char, Char)
+[(False, '0'), (True, '1')] :: [(Bool, Char)]
+([False, True], ['0', '1']) :: ([Bool], [Char])
+[tail, init, reverse]       :: [[a] -> [a]]
---
+++ chap3/chap3_4.hs
@@ -0,0 +1,7 @@
+-- Why is it not feasible in general for function
+-- types to be instances of the 'Eq' class?
+
+-- Ans : Because it is not feasible in general to compare two 
+--       functions for equality.
+--       Two functions of same type are equal if they always 
+--       return equal results for equal arguments.
